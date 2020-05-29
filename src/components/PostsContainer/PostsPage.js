f8e1572f3c6cf598488cec97be33b01884399096
kevin thew <kevthew@outlook.com>
2020-05-29 17:16:31 -0600

    finally finished mvp

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,28 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
+// import CommentSection from '../CommentSection/CommentSectionContainer'
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-
-const PostsPage = () => {
+import data from "../../dummy-data";
+// console.log(data)
+const PostsPage = (props) => {
   // set up state for your data
+  const [postData] = useState(data);
+  // console.log(postData);
+  // console.log(props);
+  
+  
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        postData.map( (post) =>(
+          <div className='post-tunnel' key={post.timestamp}>
+            <Post post={post}/>
+          </div>
+        ))
+     }
     </div>
   );
 };
